4c7f20e0bcf451f711c4d45aa8fac0e98256f48a
Franck Galpin <franck.galpin@interdigital.com>
2024-06-14 16:24:37 +0000

    remove spurious warning in gcc 12 and 13

--- sadl/dimensions.h
+++ sadl/dimensions.h
@@ -60,7 +60,14 @@ struct Dimensions
   int     size() const { return m_s; }
   int64_t nbElements() const
   {
+#if __GNUC__
+#pragma GCC diagnostic push                             // from gcc 12... to remove later 
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
     return std::accumulate(m_v, m_v + m_s, (int64_t) 1, [](int64_t a, int64_t b) { return a * b; });
+#if __GNUC__
+#pragma GCC diagnostic pop
+#endif
   }
   int            operator[](int k) const { return m_v[k]; }
   int &          operator[](int k) { return m_v[k]; }
