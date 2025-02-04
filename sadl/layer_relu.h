7efff84c2ddd6c59c1b793e1433ea2d53e812c48
Sam Eadie <seadie@qti.qualcomm.com>
2025-02-04 11:01:26 +0000

    Simd relu

--- sadl/layer_relu.h
+++ sadl/layer_relu.h
@@ -33,6 +33,11 @@
 #pragma once
 #include "layer.h"
 
+
+#if __AVX512BW__ || __AVX2__
+#include <immintrin.h>
+#endif
+
 namespace sadl
 {
 namespace layers
@@ -59,6 +64,47 @@ template<typename T> bool Relu<T>::apply(std::vector<Tensor<T> *> &in)
   swap(*in[0], m_out);
   for (auto &x: m_out)
     x = (x < 0) ? 0 : x;
+  return true;
+}
+
+template<> inline bool Relu<int16_t>::apply(std::vector<Tensor<int16_t> *> &in)
+{
+  assert(in.size() == 1);
+  assert(in[0]->dims() == m_out.dims());
+  swap(*in[0], m_out);
+  const int size = static_cast<int>(m_out.size());
+
+#if __AVX512BW__ || __AVX2__
+  int16_t* data = m_out.data();
+#if __AVX512BW__
+  const int simdSize = static_cast<int>(size - (size % 32));  // Process 32 elements per iteration
+  __m512i zeros = _mm512_setzero_si512();
+  for (int i = 0; i < simdSize; i += 32)
+  {
+      __m512i x = _mm512_loadu_si512(reinterpret_cast<const __m512i*>(data + i));
+      __m512i result = _mm512_max_epi16(x, zeros);
+      _mm512_storeu_si512(reinterpret_cast<__m512i*>(data + i), result);
+  }
+#elif __AVX2__
+  const int simdSize = static_cast<int>(size - (size % 16));  // Process 16 elements per iteration
+  __m256i zeros = _mm256_setzero_si256();
+  for (int i = 0; i < simdSize; i += 16)
+  {   
+      __m256i x = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(data + i));   
+      __m256i result = _mm256_max_epi16(x, zeros);
+      _mm256_storeu_si256(reinterpret_cast<__m256i*>(data + i), result);
+  }
+#endif
+
+  // Handle remaining elements without branching
+  for (int i = simdSize; i < size; ++i)
+  {
+#else
+  for (int i = 0; i < size; ++i) 
+  {
+#endif
+    m_out[i] = (m_out[i] < 0) ? 0 : m_out[i];
+  }
   return true;
 }
 
